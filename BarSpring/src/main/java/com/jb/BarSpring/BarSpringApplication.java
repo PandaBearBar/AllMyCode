40122530f730a512947df884305e99d5f142598c
PandaBearBar <97035109+PandaBearBar@users.noreply.github.com>
2022-09-14 17:06:48 +0300

    its heavy boy

---
+++ BarSpring/src/main/java/com/jb/BarSpring/BarSpringApplication.java
@@ -0,0 +1,15 @@
+package com.jb.BarSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BarSpringApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(BarSpringApplication.class, args);
+	}
+
+}
